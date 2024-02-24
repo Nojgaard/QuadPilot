c37f7c26452c1cf1c5dd6fe8950e198be575c14a
Nikolai Nojgaard <nnoej10@gmail.com>
2024-02-24 11:27:52 +0100

    Print ypr info in output imu

--- src/main/output-imu.cpp
+++ src/main/output-imu.cpp
@@ -2,11 +2,23 @@
 #include <IMU.h>
 
 IMU imu;
+Vector3f ypr;
+Vector3i16 acceleration;
 
 void setup() {
+  Serial.begin(9600);
   imu.initialize();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  uint8_t success = imu.read(ypr, acceleration);
+  if (success != 0) {
+    return;
+  }
+  Serial.print("ypr\t");
+  Serial.print(ypr.x * 180/M_PI);
+  Serial.print("\t");
+  Serial.print(ypr.y * 180/M_PI);
+  Serial.print("\t");
+  Serial.println(ypr.z * 180/M_PI);
 }
